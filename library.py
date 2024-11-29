07356de4e3327ac95d15206bb3fb9b109675f18d
STAeroIntern <132527058+STAeroIntern@users.noreply.github.com>
2024-11-29 14:22:53 +0800

    Update library.py

--- library.py
+++ library.py
@@ -11,20 +11,7 @@ import time
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import plotly.express as px
-# from sklearn.svm import LinearSVC
-# from sklearn.datasets import load_iris
-# from sklearn.datasets import make_classification
-# from sklearn.model_selection import train_test_split
-# from sklearn.model_selection import cross_val_score
-# from sklearn.metrics import confusion_matrix
-# from sklearn.metrics import classification_report
-import geopy.distance
-# from sklearn.preprocessing import MinMaxScaler,StandardScaler
-# from sklearn.decomposition import PCA
-# from imblearn.over_sampling import RandomOverSampler,SMOTE
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.datasets import make_classification
-from jira import JIRA 
+#from jira import JIRA 
 import streamlit as st
 import configparser
 from PIL import Image
@@ -32,4 +19,3 @@ from openpyxl import Workbook
 from openpyxl.drawing.image import Image
 from io import BytesIO
 import glob
-from multiprocessing import Pool
